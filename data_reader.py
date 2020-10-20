e20defc518bac956393f9e870c8bec0f504f5747
MCruces-fz <mcsquared.fz@gmail.com>
2020-10-20 13:42:01 +0200

    Completed Task

--- data_reader.py
+++ data_reader.py
@@ -31,9 +31,9 @@ def decimal_doy(do: int, ho: int, mi: int, se: int):
     :param se: Seconds
     :return: Doy of the year with time in DOY decimals.
     """
-    mi += se/60.
-    ho += mi/60.
-    return do + ho/24.
+    mi += se / 60.
+    ho += mi / 60.
+    return do + ho / 24.
 
 
 def rom2arab(roman: str):
@@ -125,15 +125,27 @@ meteo_codes = {
     }
 }
 
+military_tz = {  # Military Time Zones
+    "ALFA": 1,
+    "ZULU": 0,
+    "NOVEMBER": -1,
+    "OSCAR": -2,
+    "PAPA": -3
+}
+
 # dfs = pd.read_excel("hesp_rafaelhermida_2019/2.Noviembre 2019.xlsx", sheet_name=None)
 
 data_dir = join_path(ROOT_DIR, "hesp_rafaelhermida_2019")
 
 raw_data = {}
+pos_data = None  # For avoiding the warning: "pos_data may be undefined"
 for file in os.listdir(data_dir):
-    if file.endswith(".xlsx") and not file.startswith("._"):
+    if file.endswith(".xlsx") and not file.startswith("Pos"):
         file_path = join_path(data_dir, file)
         raw_data[f"{file[:-5]}"] = pd.read_excel(file_path, sheet_name=None, usecols="A:O")
+    elif file.endswith(".xlsx") and file.startswith("Pos"):
+        file_path = join_path(data_dir, file)
+        pos_data = pd.read_excel(file_path, sheet_name="Hoja1", usecols="A:D")
 
 # print(raw_data["2.Noviembre 2019"]["Dia 15"])
 
@@ -147,8 +159,9 @@ except Exception as e:
 print("doyhhmmss\tdoy.ddd\tPress.\tTemp.\tHum.")
 
 final_data = np.zeros([0, 5])
-for i in raw_data:
-    for j in raw_data[i]:
+
+for i in raw_data:  # Iterate over dictionary of Pandas Dataframes
+    for j in raw_data[i]:  # Iterate over Pandas Dataframe
         doy = get_doy(i, j)
         if doy is None:
             continue
@@ -167,6 +180,8 @@ for i in raw_data:
                 except Exception as e:  # Except all the rest of the trash
                     print(f"{e.__doc__}")
                     continue
+            if hour == 24:
+                hour = 0
             minute, second = 0, 0
             doyhhmmss = int(f"{doy:03d}{hour:02d}{minute:02d}{second:02d}")
             doy_ddd = decimal_doy(doy, hour, minute, second)
@@ -185,6 +200,45 @@ for i in raw_data:
             row = np.hstack((doyhhmmss, doy_ddd, pressure, temperature, humidity))
             final_data = np.vstack((final_data, row))
 
+f_pos_data = np.zeros([0, 3])
+for i in pos_data.iterrows():
+    row = i[1]  # Tuples (row number, row data)
+    hh, fh, lat, lon = row["HUSO HORARIO"], \
+                       row["FECHA / HORA"], \
+                       row["LATITUD (N + ; S -)"], \
+                       row["LONGITUD (E + ; W -)"]
+    if fh.second >= 59:
+        t_delta = datetime.timedelta(seconds=1)
+        fh += t_delta  # It sums one second if it is near to the next minute
 
-np.savetxt(fname="doyhhmmss-doy_ddd-pressure-temperature-humidity.txt", X=final_data, fmt='%09d %03.5f %.2f %02.2f %.3f')
+    # ============== GMT Correction ============== #
+    fh += datetime.timedelta(hours=-military_tz[hh])
+    # ============================================ #
 
+    datime = float(f"{fh.dayofyear}{fh.hour:02d}{fh.minute:02d}{fh.second:02d}")
+    try:
+        lat, lon = float(lat), float(lon)
+    except ValueError:
+        lat, lon = np.nan, np.nan
+    line = np.hstack((datime, lat, lon))
+    f_pos_data = np.vstack((f_pos_data, line))
+
+df1 = pd.DataFrame(final_data[:, 1:],
+                   index=final_data[:, 0].astype(np.int),
+                   columns=["DOY dec", "Pressure", "Temperature", "Humidity"])
+
+df2 = pd.DataFrame(f_pos_data[:, 1:],
+                   index=f_pos_data[:, 0].astype(np.int),
+                   columns=["Latitude", "Longitude"])
+
+df1 = df1.loc[~df1.index.duplicated(keep='first')]
+df2 = df2.loc[~df2.index.duplicated(keep='first')]
+
+final_df = pd.concat([df1, df2], axis=1, sort=True)
+
+final_df.to_json(join_path(data_dir, "TristanHespData.json"))
+
+# save2TXT = final_df.copy()
+# np.savetxt(fname="doyhhmmss-doy_ddd-pressure-temperature-humidity.txt",
+#            X=save2TXT,
+#            fmt='%09d %03.5f %.2f %02.2f %.3f')
